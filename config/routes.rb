f5bb55e2ee6c84faa8f7f83c1246d7da197a2342
Cassandra Mansiamina <mansiamina.cassandra@hotmail.fr>
2021-02-23 11:37:11 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :kids
+    resources :user_reviews, only: [:index, :new, :edit, :update, :destroy]
+  end
+  resources :games, only: [:index, :show]
 end
