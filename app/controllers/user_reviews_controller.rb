46b89806acf278b9b7c7505bbb2360026fba516d
Cassandra Mansiamina <mansiamina.cassandra@hotmail.fr>
2021-02-23 15:47:04 +0100

    add controller, policy and pundit user review model

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -1,41 +1,50 @@
 class UserReviewsController < ApplicationController
-  def index
-    @game = Game.find(params[:game_id])
-
-  end
-
   def new
-    @user_review = UserReviews.new
+    @user_review = UserReview.new
     authorize @user_review
     @game = Game.find(params[:game_id])
-    raise
   end
 
   def create
-    @user_review = UserReviews.new(user_reviews_params)
+    @user_review = UserReview.new(user_reviews_params)
     authorize @user_review
     @game = Game.find(params[:game_id])
     @user_review.game = @game
     @user_review.user = current_user
-    if @user_reviews.save
-      flash[:notice] = 'Votre commentaire a bien été ajouté'
+    if @user_review.save
+      # flash[:notice] = 'Votre commentaire a bien été ajouté!'
       redirect_to game_path
     else
-      flash[:notice] = 'Il y a une erreur, veuillez réessayer'
+      # flash[:notice] = 'Il y a une erreur, veuillez réessayer'
       render :new
     end
   end
 
   def edit
+    set_user_review
+    authorize @user_review
   end
 
   def update
+    set_user_review
+    authorize @user_review
+    @user_review.update(user_reviews_params)
+    redirect_to game_path(@user_review)
   end
 
   def destroy
+    set_user_review
+    authorize @user_review
+    @user_review.destroy
+    # flash[:notice] = 'Votre commentaire a bien été supprimé!'
+    redirect_to game_path
   end
 
   private
+
+  def set_user_review
+    @user_review = UserReview.find(params[:id])
+  end
 
   def user_reviews_params
     params.require(:user_reviews).permit(:age, :title, :description, :rating, :user_id, :game_id)
--- app/policies/user_review_policy.rb
+++ app/policies/user_review_policy.rb
@@ -5,4 +5,23 @@ class UserReviewPolicy < ApplicationPolicy
     end
   end
 
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end
