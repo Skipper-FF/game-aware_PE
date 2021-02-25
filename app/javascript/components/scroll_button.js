bba5cf6152ed9a744045b3cea96346877ce9d109
Timothe Serrut <tserrut@gmail.com>
2021-02-25 15:12:58 +0100

    finished navbar

---
+++ app/javascript/components/scroll_button.js
@@ -0,0 +1,17 @@
+
+// const scrollButton = () => {
+//   const navbarButton = document.querySelector("#navbar-button");
+
+
+//   if(navbarButton) {
+//     navbarButton.addEventListener("click", (event) => {
+//       setTimeout(() => {
+//         window.scrollTo({top:document.body.scrollHeight, behavior: 'smooth'});
+//       }, 250)
+
+//     });
+
+//   }
+// }
+
+// export {scrollButton};
