011536b133f21f7b9d4ea27d4c900e367302222e
Cassandra Mansiamina <mansiamina.cassandra@hotmail.fr>
2021-02-26 15:28:23 +0100

    fix controller and add front

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -1,4 +1,9 @@
 class UserReview < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :age, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
