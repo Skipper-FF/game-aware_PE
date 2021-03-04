f12118a1ee9cf6f126fdfaee95dedb2230d68f41
Timothe Serrut <tserrut@gmail.com>
2021-03-04 12:09:46 +0100

    almost done metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   # end
 
   def default_url_options
-    { host: ENV["www.game-aware.com"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
   private
